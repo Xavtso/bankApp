36b8f6639a3b372908cb9a859a7519aab384b5d8
Xavtso <93160614+Xavtso@users.noreply.github.com>
2022-10-24 00:04:08 +0300

    Update script.js

--- script.js
+++ script.js
@@ -81,6 +81,25 @@ const account4 = {
   locale: 'en-US',
 };
 
+const account5 = {
+  owner: 'Ivanna Barabash',
+  movements: [600, 1000, 8000, 80, 90, 800, 9000, 700, 50, 90],
+  interestRate: 2,
+  pin: 1818, 
+  movementsDates: [
+    '2022-11-01T13:15:33.035Z',
+    '2022-11-30T09:48:16.867Z',
+    '2022-12-25T06:04:23.907Z',
+    '2022-01-25T14:18:46.235Z',
+    '2022-02-05T16:33:06.386Z',
+    '2022-04-10T14:43:26.374Z',
+    '2022-06-25T18:49:59.371Z',
+    '2022-07-26T12:01:20.894Z',
+  ],
+  currency: 'USD',
+  locale: 'en-US',
+};
+
 const accounts = [account1, account2, account3, account4];
 
 // Elements
